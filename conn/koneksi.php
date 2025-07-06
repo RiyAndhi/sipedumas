f803339e981d2a32112b90e89a8b33b876393ed6
RiyAndhi <128784894+RiyAndhi@users.noreply.github.com>
2025-07-06 17:42:13 +0700

    Delete conn directory

--- conn/koneksi.php
+++
@@ -1,3 +0,0 @@
-<?php $koneksi=mysqli_connect("localhost","root","","pengaduanmasyarakat");
-# $koneksi=mysqli_connect("localhost","yhdsrgnb_user_sipe","user_sipe","yhdsrgnb_pengaduanmasyarakat");
-?>
